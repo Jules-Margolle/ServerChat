4703597006fad3582ca60c6e201e35ddebcd546b
Zuuules <ju.margo62630@gmail.com>
2024-03-22 12:44:19 +0100

    init mac

---
+++ src/main/java/com/julesmargolle/ServerChat/App.java
@@ -0,0 +1,13 @@
+package com.julesmargolle.ServerChat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
